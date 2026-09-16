a29188967a2d1e269c80243d4514a599ccb4389b
gnumaru <gnumaru@gnumaru.com>
2026-09-16 10:17:17 -0300

    faltou

--- servers/rendering/rendering_server.cpp
+++ servers/rendering/rendering_server.cpp
@@ -3687,6 +3687,9 @@ void RenderingServer::init() {
 	// Number of commands that can be drawn per frame.
 	GLOBAL_DEF_RST(PropertyInfo(Variant::INT, "rendering/gl_compatibility/item_buffer_size", PROPERTY_HINT_RANGE, "128,1048576,1"), 16384);
 
+	// Godot 3 style FXAA in the GLES2 post pass (no Environment property in Godot 4).
+	GLOBAL_DEF("rendering/gl_compatibility/fxaa_enabled", false);
+
 	GLOBAL_DEF("rendering/shader_compiler/shader_cache/enabled", true);
 	GLOBAL_DEF("rendering/shader_compiler/shader_cache/compress", true);
 	GLOBAL_DEF("rendering/shader_compiler/shader_cache/use_zstd_compression", true);
